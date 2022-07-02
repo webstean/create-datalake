8d528bcff940697b62c9a5ad1fc6bc4ff37f4d3e
Andrew Webster <webstean@gmail.com>
2022-07-02 10:45:13 +1000

    add login

--- run.cmd
+++ run.cmd
@@ -1,5 +1,6 @@
 @echo off
 
+az login
 
 az group create --name data-lake --location australiasoutheast
 az group deployment create --resource-group data-lake --template-uri    https://raw.githubusercontent.com/webstean/create-datalake/master/azuredeploy.json
